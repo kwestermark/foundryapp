24206dbac9cafa4b29c963e117eea8123795cb3c
kwestermark <kwestermark21@gmail.com>
2017-10-03 17:18:30 -0400

    postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  resources :products do
-    resources :comments
-  end
+  resources :products  
 
   get 'static_pages/contact'
 
-  get 'products/index'
-
   get 'static_pages/index'
+
   root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
