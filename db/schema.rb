1aeb3f9af96688b5ba01dfec785d23eed2492d9e
kwestermark <kwestermark21@gmail.com>
2017-10-13 11:15:44 -0400

    added fields and modified views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928154133) do
+ActiveRecord::Schema.define(version: 20171008131225) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170928154133) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
+    t.integer  "price"
   end
 
 end
