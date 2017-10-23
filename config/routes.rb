432ae9a886d3506c8fca174c4deb83e9703729b2
kwestermark <kwestermark21@gmail.com>
2017-10-23 18:08:32 -0400

    4.5 Views

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
  
 
+  get 'static_pages/about'
+
   get 'static_pages/contact'
 
-  get 'static_pages/index'
-
   root 'static_pages#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
