3ce6cd749c11ba4c23abb4005b412064bcefaf52
kwestermark <kwestermark21@gmail.com>
2017-12-15 11:41:12 -0500

    email updates

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['app76734448@heroku.com'],
-  password: ENV['ycdmkp0u7855'],
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
