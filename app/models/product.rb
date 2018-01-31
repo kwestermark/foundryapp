a0a70ab8e5481adfbefab32d091a9b3bbacf4efa
kwestermark <kwestermark21@gmail.com>
2018-01-31 12:06:28 -0500

    5.7 Comment Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,16 @@
 class Product < ApplicationRecord
-
+ has_many :comments
 def self.search(search_term)
   Product.where("name LIKE ?", "%#{search_term}%")
 end
+
+def highest_rating_comment
+    comments.rating_desc.first&.rating
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first&.rating
+  end
+
 	
 end
