c4bd396678a2ff2d0063f014399a6bf7c10c3848
kwestermark <kwestermark21@gmail.com>
2018-02-26 16:48:06 -0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,9 @@ Rails.application.configure do
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+config.assets.initialize_on_precompile = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  #config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
