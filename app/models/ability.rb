28fdcc178af9dec9f166c875d7414c7493648840
kwestermark <kwestermark21@gmail.com>
2018-03-06 13:30:00 -0500

    5.10 Admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    if user.present?  # additional permissions for logged in users (they can manage their posts)
+      can :create, Comment, id: user.id
+      if user.admin?  # additional permissions for administrators
+        can :manage, :all
+      end
+    end
   end
- 
 end
