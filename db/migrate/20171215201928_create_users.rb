20aaa530eed111935701dad57fb0c4e4961afbab
kwestermark <kwestermark21@gmail.com>
2018-03-13 15:03:20 -0400

    changes

--- db/migrate/20171215201928_create_users.rb
+++ db/migrate/20171215201928_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.boolean :admin_flag
+      t.boolean :admin
 
       t.timestamps
     end
